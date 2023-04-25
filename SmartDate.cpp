72abb4e3a87713c409fbba67a16f9f13ad9691e3
Arsen <moyapochta@mail.ru>
2023-04-25 21:00:39 +0300

    Defined class SD

--- SmartDate.cpp
+++ SmartDate.cpp
@@ -1,6 +1,20 @@
 ﻿#include <iostream>
 
+class SD {
+private:
+    short day, month, year;
+public:
+    SD(short day, short month, short year);
+};
+
+SD::SD(short d, short m, short y) {
+    day = d;
+    month = m;
+    year = y;
+    std::cout << d << "." << m << "." << y;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    SD d1(25, 04, 2023);
 }
